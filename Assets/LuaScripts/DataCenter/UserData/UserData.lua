bab01abd343a7cc0526279c6a749d6da899150f3
chensijiu <997053278@qq.com>
2018-09-24 22:27:45 +0800

    2018.09.24 userdata

---
+++ Assets/LuaScripts/DataCenter/UserData/UserData.lua
@@ -0,0 +1,46 @@
+--[[
+-- added by zl @ 2018-09-24
+-- 角色数据
+--]]
+
+local RoleData = {
+    roleID = 0,             --角色唯一id
+    roleName = "",          --角色名字
+    roleHeadImage = "",     --角色头像
+    roleLevel = 0,          --等级
+    roleFighting = 0,       --战斗力
+    roleExp = 0,            --经验值
+    roleDiamonds = 0,       --钻石
+    roleGolds = 0,          --金币
+
+}
+
+local UserData = BaseClass("UserData", Singleton)
+local RoleData = DataClass("RoleData", RoleData)
+
+local function __init(self)
+	-- 所有服务器列表
+	self.servers = {}
+end
+
+-- 解析网络数据
+local function ParseServerList(self, servers)
+	self.servers = {}
+	for _,v in pairs(servers) do
+		local roleData = RoleData.New()
+        roleData.roleID = v.roleID
+        roleData.roleName = v.roleName
+        roleData.roleHeadImage = v.roleHeadImage
+        roleData.roleLevel = v.roleLevel
+        roleData.roleFighting = v.roleFighting
+        roleData.roleExp = v.roleExp
+        roleData.roleDiamonds = v.roleDiamonds
+        roleData.roleGolds = v.roleGolds
+		self.servers[roleData.roleID] = roleData
+	end
+	--DataManager:GetInstance():Broadcast(DataMessageNames.ON_SERVER_LIST_CHG, self)
+end
+
+UserData.ParseServerList = ParseServerList
+
+return UserData
